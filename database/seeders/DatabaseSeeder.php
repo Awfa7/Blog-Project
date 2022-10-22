6ecb684d4985d03946c4f0b9b71612057fdcdd73
Awfa7 <awfa1996@gmail.com>
2022-10-23 01:53:26 +0300

    Database, Design, Search

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +19,60 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        // User::truncate();
+        // Category::truncate();
+        // Post::truncate();
+
+        $user = User::factory()->create([
+            'name' => 'Awfa'
+        ]);
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
+
+        // $user = User::factory()->create();
+
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
+
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $personal->id,
+        //     'title' => 'First post',
+        //     'slug' => 'first-post',
+        //     'excerpt'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda architecto corporis perferendis alias itaque ex aliquam dolorem maxime, quos amet velit expedita dolorum quia. Officia, itaque recusandae. Molestiae, harum cupiditate.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'Second Post',
+        //     'slug' => 'second-post',
+        //     'excerpt'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda architecto corporis perferendis alias itaque ex aliquam dolorem maxime, quos amet velit expedita dolorum quia. Officia, itaque recusandae. Molestiae, harum cupiditate.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'Third Post',
+        //     'slug' => 'third-post',
+        //     'excerpt'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        //     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda architecto corporis perferendis alias itaque ex aliquam dolorem maxime, quos amet velit expedita dolorum quia. Officia, itaque recusandae. Molestiae, harum cupiditate.</p>'
         // ]);
     }
 }
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<x-layout>
+    
+@include('_posts-header')
+
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts"></x-posts-grid>
+        @else
+            <p class="text-center">No Posts yet. Please check back later.</p>
+        @endif
+    
+
+    {{--
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card />
+
+        <x-post-card />
+
+        <x-post-card />
+    </div> --}}
+</main>
+    
+    
+</x-layout>
